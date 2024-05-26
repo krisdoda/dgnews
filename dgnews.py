42da7436e8a7fcabba2252689a6d4f5f4edfccd1
krisdoda <hi@kdoda.com>
2024-05-26 18:19:37 +0200

    Made it available to "public"
    app.run(debug=True, host='0.0.0.0', port=3000)

--- dgnews.py
+++ dgnews.py
@@ -114,4 +114,4 @@ def home():
     return render_template('index.html', articles=articles)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
